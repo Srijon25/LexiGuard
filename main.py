3fe3f6db8d0eb0726257ba4d65b32c7e8a8d8272
Srijon25 <theunpredictable157@gmail.com>
2025-06-03 01:19:01 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# main.py - Entry point for LexiGuard
+import cancel_phrase_detector
+import risk_engine
+def main():
+    print("Starting LexiGuard...")
+    # main code here
+if __name__ == "__main__":
+    main()
